34f9e30fd87e51a9f4550687901207e6ef890a4a
maj-jinnah <maj.jinnah@gmail.com>
2022-11-24 00:33:54 +0600

    errro page done

---
+++ src/Pages/Shared/ErrorPage/ErrorPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../../assets/errorimg/error.jpg'
+
+const ErrorPage = () => {
+    return (
+        <div className='text-center h-screen bg-slate-200'>
+            <div className='flex flex-col items-center justify-center'>
+                <h1 className='text-4xl md:text-5xl font-semibold mt-12 mb-4'>Whoops!</h1>
+                <h1 className='text-lg md:text-2xl mb-5 font-medium'>404 Page Not Found</h1>
+                <img className='w-2/3 md:w-2/5 lg:w-2/6 rounded-xl' src={img} alt="" />
+                <p className='mt-3 text-2xl font-medium'>Looks like this page went on vacation..</p>
+                <p className='mt-4 text-lg font-medium'>Try out <Link to='/' className='text-orange-400'>homepage</Link> or <Link to='/' className='text-orange-400'>blog</Link> instead..</p>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routers/Route/Router.js
+++ src/Routers/Route/Router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main/Main";
 import Home from "../../Pages/Home/Home/Home";
+import ErrorPage from "../../Pages/Shared/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
     {
@@ -12,5 +13,9 @@ export const router = createBrowserRouter([
                 element: <Home></Home>
             }
         ]
+    },
+    {
+        path: '*',
+        element: <ErrorPage></ErrorPage>
     }
 ])
