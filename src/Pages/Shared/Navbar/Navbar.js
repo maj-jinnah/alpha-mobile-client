b0446ed607c4bdd7ae3c832067105ef558b3bd6c
maj-jinnah <maj.jinnah@gmail.com>
2022-11-24 01:09:29 +0600

    navbar added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo/logo.png'
 
 const Navbar = () => {
+
+    const menuItems =
+        <>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/blog'>Blogs</Link></li>
+            {/* {
+                user?.uid ?
+                    <>
+                        <li><Link to='/dashBoard'>DashBoard</Link></li>
+                        <li><button onClick={HandelLouOut} >Sign Out</button></li>
+                    </> :
+                    <li><Link to='/login'>LogIn</Link></li>
+            } */}
+        </>
+
     return (
-        <div>
-            <h1>this is navbar</h1>
+        <div className="navbar bg-base-200 text-black flex justify-between">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-ghost normal-case text-xl"><img className='w-11' src={logo} alt="" /><span className='ml-2'>AlphaMobile</span></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
         </div>
     );
 };
