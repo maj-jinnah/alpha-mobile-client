4e503801dc4edff45a969ffc0d296200d445c615
maj-jinnah <maj.jinnah@gmail.com>
2022-11-27 16:55:19 +0600

    dash boars page is creted

--- src/Layouts/DashBoard/DashBoardLayout.js
+++ src/Layouts/DashBoard/DashBoardLayout.js
@@ -16,7 +16,11 @@ const DashBoardLayout = () => {
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                         <small>Click the option below</small>
                         <li><Link to='/dashboard/myorders'>My Orders</Link></li>
-                        <li><Link>Sidebar Item 2</Link></li>
+                        <li><Link to='/dashboard/addaproduct'> Add A product</Link></li>
+                        <li><Link to='/dashboard/myproduct'>My Products</Link></li>
+                        <li><Link to='/dashboard/allbuyers'>All Buyers</Link></li>
+                        <li><Link to='/dashboard/allsellers'>All Sellers</Link></li>
+                        <li><Link to='/dashboard/reporteditems'>Reported Items</Link></li>
                     </ul>
 
                 </div>
--- src/Routers/Route/Router.js
+++ src/Routers/Route/Router.js
@@ -2,7 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import DashBoardLayout from "../../Layouts/DashBoard/DashBoardLayout";
 import Main from "../../Layouts/Main/Main";
 import Blogs from "../../Pages/Blogs/Blogs";
+import AddAProduct from "../../Pages/DashBoard/Add a Product/AddAProduct";
+import AllBuyers from "../../Pages/DashBoard/All Buyers/AllBuyers";
+import AllSellers from "../../Pages/DashBoard/All Sellers/AllSellers";
+import MyProducts from "../../Pages/DashBoard/My Products/MyProducts";
 import MyOrders from "../../Pages/DashBoard/MyOrders/MyOrders";
+import ReportedItems from "../../Pages/DashBoard/Reported Items/ReportedItems";
 import Home from "../../Pages/Home/Home/Home";
 import Phones from "../../Pages/Phones/Phones";
 import ErrorPage from "../../Pages/Shared/ErrorPage/ErrorPage";
@@ -46,6 +51,26 @@ export const router = createBrowserRouter([
             {
                 path: '/dashboard/myorders',
                 element: <MyOrders></MyOrders>
+            },
+            {
+                path: '/dashboard/addaproduct',
+                element: <AddAProduct></AddAProduct>
+            },
+            {
+                path: '/dashboard/myproduct',
+                element: <MyProducts></MyProducts>
+            },
+            {
+                path: '/dashboard/allbuyers',
+                element: <AllBuyers></AllBuyers>
+            },
+            {
+                path: '/dashboard/allsellers',
+                element: <AllSellers></AllSellers>
+            },
+            {
+                path: '/dashboard/reporteditems',
+                element: <ReportedItems></ReportedItems>
             }
         ]
     },
